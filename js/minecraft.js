7b096a3271085d6bf53fc27a5cf8a888975a3bc0
ItamiKyojin <44494007+ItamiKyojin@users.noreply.github.com>
2021-12-28 22:07:13 +0100

    added css and js for WebGL + Minecraft

---
+++ js/minecraft.js
@@ -0,0 +1,17 @@
+"use strict"
+
+window.onload = (() => {
+    let canvas = document.getElementById('minecraft-surface');
+    let gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if (!gl) {
+        alert('Your browser does not support WebGL');
+    }
+
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+})
