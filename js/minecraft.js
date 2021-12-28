4523b54939b46f7550ff570169634033427253c1
ItamiKyojin <44494007+ItamiKyojin@users.noreply.github.com>
2021-12-29 00:13:02 +0100

    made triangle spin using gl-matrix.js

--- js/minecraft.js
+++ js/minecraft.js
@@ -3,13 +3,16 @@
 let vertexShaderText = [
     'precision mediump float;',
     '',
-    'attribute vec2 vertPosition;',
+    'attribute vec3 vertPosition;',
     'attribute vec3 vertColor;',
     'varying vec3 fragColor;',
+    'uniform mat4 mWorld;',
+    'uniform mat4 mView;',
+    'uniform mat4 mProj;',
     '',
     'void main() {',
     '   fragColor = vertColor;',
-    '   gl_Position = vec4(vertPosition, 0.0, 1.0);',
+    '   gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
     '}'
 ].join('\n');
 
@@ -81,10 +84,10 @@ window.onload = (() => {
 
     // create buffer
     let triangleVertices = [
-     // X, Y,       R, G, B
-        0.0, 0.5,   1.0, 1.0, 0.0,
-        -0.5, -0.5, 0.7, 0.0, 1.0,
-        0.5, -0.5,  0.1, 1.0, 0.6
+     // X, Y, Z,           R, G, B
+        0.0, 0.5, 0.0,     1.0, 1.0, 0.0,
+        -0.5, -0.5, 0.0,   0.7, 0.0, 1.0,
+        0.5, -0.5, 0.0,    0.1, 1.0, 0.6
     ];
 
     let triangleVertexBufferObject = gl.createBuffer();
@@ -98,7 +101,7 @@ window.onload = (() => {
         2, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE, // Data is normalized
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
         0 // Offset from the beginning of a single vertex to this attribute
     );
     gl.vertexAttribPointer(
@@ -106,16 +109,47 @@ window.onload = (() => {
         3, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE, // Data is normalized
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-        2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
     );
 
 
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(colorAttribLocation);
 
+    // Tell OpenGL state machine which program should be active
+    gl.useProgram(program);
+
+    let matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+    let matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+    let matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+
+    let worldMatrix = new Float32Array(16);
+    let viewMatrix = new Float32Array(16);
+    let projMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(worldMatrix);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0])
+    glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
 
     // main render loop
-    gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLES, 0, 3)
+    let identityMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(identityMatrix);
+    let angle = 0;
+    let loop = () => {
+        angle = performance.now() / 1000 / 6 * 2 * Math.PI; // 1 rotation every 6 seconds
+        glMatrix.mat4.rotate(worldMatrix, identityMatrix, angle, [0, 1, 0]);
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+        gl.clearColor(0.75, 0.85, 0.8, 1.0);
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, 3)
+
+        requestAnimationFrame(loop);
+    };
+    requestAnimationFrame(loop);
 })
