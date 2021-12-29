ffba75d80f5df3d61ca56ccb0872d21773df6f67
ItamiKyojin <44494007+ItamiKyojin@users.noreply.github.com>
2021-12-29 01:16:45 +0100

    updated minecraft.js to show rotating cube

--- js/minecraft.js
+++ js/minecraft.js
@@ -45,6 +45,10 @@ window.onload = (() => {
 
     gl.clearColor(0.75, 0.85, 0.8, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.frontFace(gl.CCW);
+    gl.cullFace(gl.BACK);
 
 
     // create shaders
@@ -83,22 +87,84 @@ window.onload = (() => {
 
 
     // create buffer
-    let triangleVertices = [
-     // X, Y, Z,           R, G, B
-        0.0, 0.5, 0.0,     1.0, 1.0, 0.0,
-        -0.5, -0.5, 0.0,   0.7, 0.0, 1.0,
-        0.5, -0.5, 0.0,    0.1, 1.0, 0.6
+    let boxVertices = [
+        //X, Y, Z,         R, G, B
+        // Top
+        -1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+        -1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+        1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+        1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+
+        // Left
+        -1.0, 1.0, 1.0,     0.75, 0.25, 0.5,
+        -1.0, -1.0, 1.0,    0.75, 0.25, 0.5,
+        -1.0, -1.0, -1.0,   0.75, 0.25, 0.5,
+        -1.0, 1.0, -1.0,    0.75, 0.25, 0.5,
+
+        // Right
+        1.0, 1.0, 1.0,     0.25, 0.25, 0.75,
+        1.0, -1.0, 1.0,    0.25, 0.25, 0.75,
+        1.0, -1.0, -1.0,   0.25, 0.25, 0.75,
+        1.0, 1.0, -1.0,    0.25, 0.25, 0.75,
+
+        // Front
+        1.0, 1.0, 1.0,     1.0, 0.0, 0.15,
+        1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+        -1.0, -1.0, 1.0,   1.0, 0.0, 0.15,
+        -1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+        // Back
+        1.0, 1.0, -1.0,     0.0, 1.0, 0.15,
+        1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+        -1.0, -1.0, -1.0,   0.0, 1.0, 0.15,
+        -1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+        // Bottom
+        -1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+        -1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+        1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+        1.0, -1.0, -1.0,    0.5, 0.5, 1.0
     ];
 
-    let triangleVertexBufferObject = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+    let boxIndices = [
+        // Top
+        0, 1, 2,
+        0, 2, 3,
+
+        // Left
+        5, 4, 6,
+        6, 4, 7,
+
+        // Right
+        8, 9, 10,
+        8, 10, 11,
+
+        // Front
+        13, 12, 14,
+        15, 14, 12,
+
+        // Back
+        16, 17, 18,
+        16, 18, 19,
+
+        // Bottom
+        21, 20, 22,
+        22, 20, 23
+    ];
+
+    let boxVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+
+    let boxIndexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
     let positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     let colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
     gl.vertexAttribPointer(
         positionAttribLocation, // Attribute location
-        2, // Number of elements per attribute
+        3, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE, // Data is normalized
         6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
@@ -128,12 +194,16 @@ window.onload = (() => {
     let viewMatrix = new Float32Array(16);
     let projMatrix = new Float32Array(16);
     glMatrix.mat4.identity(worldMatrix);
-    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0])
-    glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0])
+    // canvas.width / canvas.height causes mild issues / inconsistencies ... I think?
+    glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
 
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
     gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
     gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+    let xRotationMatrix = new Float32Array(16);
+    let yRotationMatrix = new Float32Array(16);
 
 
     // main render loop
@@ -142,12 +212,14 @@ window.onload = (() => {
     let angle = 0;
     let loop = () => {
         angle = performance.now() / 1000 / 6 * 2 * Math.PI; // 1 rotation every 6 seconds
-        glMatrix.mat4.rotate(worldMatrix, identityMatrix, angle, [0, 1, 0]);
+        glMatrix.mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
+        glMatrix.mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
+        glMatrix.mat4.mul(worldMatrix, xRotationMatrix, yRotationMatrix);
         gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
         gl.clearColor(0.75, 0.85, 0.8, 1.0);
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0, 3)
+        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 
         requestAnimationFrame(loop);
     };
