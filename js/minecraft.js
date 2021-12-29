515c6a8b1fda02ec6ee35aede95330c5c7fdaccd
ItamiKyojin <44494007+ItamiKyojin@users.noreply.github.com>
2021-12-29 02:31:20 +0100

    added smooth_stone textures to cube

--- js/minecraft.js
+++ js/minecraft.js
@@ -4,14 +4,14 @@ let vertexShaderText = [
     'precision mediump float;',
     '',
     'attribute vec3 vertPosition;',
-    'attribute vec3 vertColor;',
-    'varying vec3 fragColor;',
+    'attribute vec2 vertTexCoord;',
+    'varying vec2 fragTexCoord;',
     'uniform mat4 mWorld;',
     'uniform mat4 mView;',
     'uniform mat4 mProj;',
     '',
     'void main() {',
-    '   fragColor = vertColor;',
+    '   fragTexCoord = vertTexCoord;',
     '   gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
     '}'
 ].join('\n');
@@ -19,10 +19,11 @@ let vertexShaderText = [
 let fragmentShaderText = [
     'precision mediump float;',
     '',
-    'varying vec3 fragColor;',
+    'varying vec2 fragTexCoord;',
+    'uniform sampler2D sampler;',
     '',
     'void main() {',
-    '   gl_FragColor = vec4(fragColor, 1.0);',
+    '   gl_FragColor = texture2D(sampler, fragTexCoord);',
     '}'
 ].join('\n');
 
@@ -90,40 +91,40 @@ window.onload = (() => {
     let boxVertices = [
         //X, Y, Z,         R, G, B
         // Top
-        -1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
-        -1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
-        1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
-        1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+        -1.0, 1.0, -1.0,    1, 1,
+        -1.0, 1.0, 1.0,     1, 0,
+        1.0, 1.0, 1.0,      0, 0,
+        1.0, 1.0, -1.0,     0, 1,
 
         // Left
-        -1.0, 1.0, 1.0,     0.75, 0.25, 0.5,
-        -1.0, -1.0, 1.0,    0.75, 0.25, 0.5,
-        -1.0, -1.0, -1.0,   0.75, 0.25, 0.5,
-        -1.0, 1.0, -1.0,    0.75, 0.25, 0.5,
+        -1.0, 1.0, 1.0,     1, 0,
+        -1.0, -1.0, 1.0,    1, 1,
+        -1.0, -1.0, -1.0,   0, 1,
+        -1.0, 1.0, -1.0,    0, 0,
 
         // Right
-        1.0, 1.0, 1.0,     0.25, 0.25, 0.75,
-        1.0, -1.0, 1.0,    0.25, 0.25, 0.75,
-        1.0, -1.0, -1.0,   0.25, 0.25, 0.75,
-        1.0, 1.0, -1.0,    0.25, 0.25, 0.75,
+        1.0, 1.0, 1.0,      0, 0,
+        1.0, -1.0, 1.0,     0, 1,
+        1.0, -1.0, -1.0,    1, 1,
+        1.0, 1.0, -1.0,     1, 0,
 
         // Front
-        1.0, 1.0, 1.0,     1.0, 0.0, 0.15,
-        1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-        -1.0, -1.0, 1.0,   1.0, 0.0, 0.15,
-        -1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+        1.0, 1.0, 1.0,      1, 0,
+        1.0, -1.0, 1.0,     1, 1,
+        -1.0, -1.0, 1.0,    0, 1,
+        -1.0, 1.0, 1.0,     0, 0,
 
         // Back
-        1.0, 1.0, -1.0,     0.0, 1.0, 0.15,
-        1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-        -1.0, -1.0, -1.0,   0.0, 1.0, 0.15,
-        -1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+        1.0, 1.0, -1.0,     0, 0,
+        1.0, -1.0, -1.0,    0, 1,
+        -1.0, -1.0, -1.0,   1, 1,
+        -1.0, 1.0, -1.0,    1, 0,
 
         // Bottom
-        -1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
-        -1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
-        1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
-        1.0, -1.0, -1.0,    0.5, 0.5, 1.0
+        -1.0, -1.0, -1.0,   1, 0,
+        -1.0, -1.0, 1.0,    1, 1,
+        1.0, -1.0, 1.0,     0, 1,
+        1.0, -1.0, -1.0,    0, 0
     ];
 
     let boxIndices = [
@@ -161,27 +162,38 @@ window.onload = (() => {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
     let positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-    let colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    let texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
     gl.vertexAttribPointer(
         positionAttribLocation, // Attribute location
         3, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE, // Data is normalized
-        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
         0 // Offset from the beginning of a single vertex to this attribute
     );
     gl.vertexAttribPointer(
-        colorAttribLocation, // Attribute location
-        3, // Number of elements per attribute
+        texCoordAttribLocation, // Attribute location
+        2, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE, // Data is normalized
-        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
         3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
     );
 
 
     gl.enableVertexAttribArray(positionAttribLocation);
-    gl.enableVertexAttribArray(colorAttribLocation);
+    gl.enableVertexAttribArray(texCoordAttribLocation);
+
+
+    // create texture
+    let boxTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, document.getElementById('smooth_stone-image'));
+    gl.bindTexture(gl.TEXTURE_2D, null);
 
     // Tell OpenGL state machine which program should be active
     gl.useProgram(program);
@@ -219,6 +231,10 @@ window.onload = (() => {
 
         gl.clearColor(0.75, 0.85, 0.8, 1.0);
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+        gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+        gl.activeTexture(gl.TEXTURE0);
+
         gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 
         requestAnimationFrame(loop);
