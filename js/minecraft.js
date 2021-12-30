88c51cb771424d4319acd0b3fd9db8357fe5456a
ItamiKyojin <44494007+ItamiKyojin@users.noreply.github.com>
2021-12-30 17:05:59 +0100

    major updates to minecraft.js
    -added option to use multiple assets
    -textures now use only block-atlas
    -facing of blocks can be somewhat changed
    -blocks can use different textures for different surfaces

--- js/minecraft.js
+++ js/minecraft.js
@@ -88,81 +88,71 @@ window.onload = (() => {
 
 
     // create buffer
-    let coords = [
-        [-1, -1, -1],
-        [-1, -1, 0],
-        [-1, -1, 1],
-        [0, -1, -1],
-        [0, -1, 1],
-        [1, -1, -1],
-        [1, -1, 0],
-        [1, -1, 1],
-        [-1, 0, -1],
-        [-1, 0, 1],
-        [1, 0, -1],
-        [1, 0, 1],
-        [-1, 1, -1],
-        [-1, 1, 0],
-        [-1, 1, 1],
-        [0, 1, -1],
-        [0, 1, 1],
-        [1, 1, -1],
-        [1, 1, 0],
-        [1, 1, 1]
+    let blocks = [
+        {'name' : 'redstone_block', 'coords' : [-1, -1, -1]},
+        {'name' : 'smooth_stone',   'coords' : [-1, -1, 0]},
+        {'name' : 'redstone_block', 'coords' : [-1, -1, 1]},
+        {'name' : 'smooth_stone',   'coords' : [0, -1, -1]},
+        {'name' : 'smooth_stone',   'coords' : [0, -1, 0]},
+        {'name' : 'smooth_stone',   'coords' : [0, -1, 1]},
+        {'name' : 'redstone_block', 'coords' : [1, -1, -1]},
+        {'name' : 'smooth_stone',   'coords' : [1, -1, 0]},
+        {'name' : 'redstone_block', 'coords' : [1, -1, 1]},
+        {'name' : 'piston', 'coords' : [0, 0, 0], 'facing' : 'top'}
     ]
 
-    let allBoxVertices = [];
-    let allBoxIndices = [];
+    let allBlockVertices = [];
+    let allBlockIndices = [];
 
     let ox = 0;
     let oy = 0;
     let oz = 0;
 
-    coords.forEach((coord, index) => {
-        ox = coord[0] * 2;
-        oy = coord[1] * 2;
-        oz = coord[2] * 2;
+    let texCoords = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+    /* row:r, column:c; r/cTop, r/cLeft, r/cRight, r/cFront, r/cBack, r/cBottom */
 
-        allBoxVertices = allBoxVertices.concat([
-            //X, Y, Z,                         U, V
-            // Top
-            -1.0 + ox, 1.0 + oy, -1.0 + oz,    1, 1,
-            -1.0 + ox, 1.0 + oy, 1.0 + oz,     1, 0,
-            1.0 + ox, 1.0 + oy, 1.0 + oz,      0, 0,
-            1.0 + ox, 1.0 + oy, -1.0 + oz,     0, 1,
+    blocks.forEach((block, index) => {
+        ox = block.coords[0] * 2;
+        oy = block.coords[1] * 2;
+        oz = block.coords[2] * 2;
 
-            // Left
-            -1.0 + ox, 1.0 + oy, 1.0 + oz,     1, 0,
-            -1.0 + ox, -1.0 + oy, 1.0 + oz,    1, 1,
-            -1.0 + ox, -1.0 + oy, -1.0 + oz,   0, 1,
-            -1.0 + ox, 1.0 + oy, -1.0 + oz,    0, 0,
+        texCoords.forEach((_, index) => {
+            texCoords[index] = -1;
+        })
 
-            // Right
-            1.0 + ox, 1.0 + oy, 1.0 + oz,      0, 0,
-            1.0 + ox, -1.0 + oy, 1.0 + oz,     0, 1,
-            1.0 + ox, -1.0 + oy, -1.0 + oz,    1, 1,
-            1.0 + ox, 1.0 + oy, -1.0 + oz,     1, 0,
+        // acacia planks: row 5, column 11
+        switch (block.name) {
+            case 'smooth_stone':
+                texCoords[0] = 20;
+                texCoords[1] = 14;
+                break;
+            case 'redstone_block':
+                texCoords[0] = 18;
+                texCoords[1] = 30;
+                break;
+            case 'piston':
+                texCoords[0] = 17;
+                texCoords[1] = 4;
 
-            // Front
-            1.0 + ox, 1.0 + oy, 1.0 + oz,      1, 0,
-            1.0 + ox, -1.0 + oy, 1.0 + oz,     1, 1,
-            -1.0 + ox, -1.0 + oy, 1.0 + oz,    0, 1,
-            -1.0 + ox, 1.0 + oy, 1.0 + oz,     0, 0,
+                texCoords[2] = 17;
+                texCoords[3] = 3;
+                texCoords[4] = 17;
+                texCoords[5] = 3;
+                texCoords[6] = 17;
+                texCoords[7] = 3;
+                texCoords[8] = 17;
+                texCoords[9] = 3;
 
-            // Back
-            1.0 + ox, 1.0 + oy, -1.0 + oz,     0, 0,
-            1.0 + ox, -1.0 + oy, -1.0 + oz,    0, 1,
-            -1.0 + ox, -1.0 + oy, -1.0 + oz,   1, 1,
-            -1.0 + ox, 1.0 + oy, -1.0 + oz,    1, 0,
+                texCoords[10] = 17;
+                texCoords[11] = 1;
+                break;
+        }
 
-            // Bottom
-            -1.0 + ox, -1.0 + oy, -1.0 + oz,   1, 0,
-            -1.0 + ox, -1.0 + oy, 1.0 + oz,    1, 1,
-            1.0 + ox, -1.0 + oy, 1.0 + oz,     0, 1,
-            1.0 + ox, -1.0 + oy, -1.0 + oz,    0, 0
-        ])
+        texCoords = rotateTexCoords(texCoords, block.facing);
 
-        allBoxIndices = allBoxIndices.concat([
+        allBlockVertices = allBlockVertices.concat(singleBlockVertices(ox, oy, oz, texCoords, block.facing))
+
+        allBlockIndices = allBlockIndices.concat([
             // Top
             0 + 24 * index, 1 + 24 * index, 2 + 24 * index,
             0 + 24 * index, 2 + 24 * index, 3 + 24 * index,
@@ -191,11 +181,11 @@ window.onload = (() => {
 
     let boxVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(allBoxVertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(allBlockVertices), gl.STATIC_DRAW);
 
     let boxIndexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(allBoxIndices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(allBlockIndices), gl.STATIC_DRAW);
 
     let positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     let texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
@@ -222,13 +212,13 @@ window.onload = (() => {
 
 
     // create texture
-    let boxTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+    let blockAtlasTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, blockAtlasTexture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, document.getElementById('smooth_stone-image'));
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, document.getElementById('block_atlas'));
     gl.bindTexture(gl.TEXTURE_2D, null);
 
     // Tell OpenGL state machine which program should be active
@@ -263,12 +253,178 @@ window.onload = (() => {
         gl.clearColor(0.75, 0.85, 0.8, 1.0);
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
-        gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+        gl.bindTexture(gl.TEXTURE_2D, blockAtlasTexture);
         gl.activeTexture(gl.TEXTURE0);
 
-        gl.drawElements(gl.TRIANGLES, allBoxIndices.length, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, allBlockIndices.length, gl.UNSIGNED_SHORT, 0);
+
+        gl.bindTexture(gl.TEXTURE_2D, null);
 
         requestAnimationFrame(loop);
     };
     requestAnimationFrame(loop);
 })
+
+function rotateTexCoords(texCoords, facing = 'top') {
+    /* row:r, column:c; r/cTop, r/cLeft, r/cRight, r/cFront, r/cBack, r/cBottom */
+
+    const TOP = 0;
+    const LEFT = 1;
+    const RIGHT = 2;
+    const FRONT = 3;
+    const BACK = 4;
+    const BOTTOM = 5;
+
+    switch(facing) {
+        case 'left':
+            // top -> left -> bottom -> right -> top
+            texCoords = shift4Faces(texCoords, [TOP, LEFT, BOTTOM, RIGHT]);
+            break;
+        case 'right':
+            // top -> right -> bottom -> left -> top
+            texCoords = shift4Faces(texCoords, [TOP, RIGHT, BOTTOM, LEFT]);
+            break;
+        case 'front':
+            // top -> front -> bottom -> back -> top
+            texCoords = shift4Faces(texCoords, [TOP, FRONT, BOTTOM, BACK]);
+            break;
+        case 'back':
+            // top -> front -> bottom -> back -> top
+            texCoords = shift4Faces(texCoords, [TOP, BACK, BOTTOM, FRONT]);
+            break;
+        case 'bottom':
+            // top -> front -> bottom -> back -> top
+            texCoords = shift4Faces(texCoords, [TOP, BOTTOM]);
+            break;
+    }
+
+    return texCoords;
+}
+
+function shift4Faces(texCoords, faces) {
+    let temp = [-1, -1];
+    let result;
+
+    if(faces[0] !== undefined &&
+        faces[1] !== undefined &&
+        faces[2] !== undefined &&
+        faces[3] !== undefined) {
+        // top -> temp
+        result = shiftFace(texCoords, faces[0], -1, temp, 'toTemp');
+        texCoords = result[0];
+        temp = result[1]
+        // right -> top
+        texCoords = shiftFace(texCoords, faces[3], faces[0])[0];
+        // bottom -> right
+        texCoords = shiftFace(texCoords, faces[2], faces[3])[0];
+        // left -> bottom
+        texCoords = shiftFace(texCoords, faces[1], faces[2])[0];
+        // temp -> left
+        texCoords = shiftFace(texCoords, -1, faces[1], temp, 'fromTemp')[0];
+    } else if(faces[0] !== undefined &&
+        faces[1] !== undefined) {
+        // top -> temp
+        result = shiftFace(texCoords, faces[0], -1, temp, 'toTemp');
+        texCoords = result[0];
+        temp = result[1]
+        // bottom -> top
+        texCoords = shiftFace(texCoords, faces[1], faces[0])[0];
+        // temp -> bottom
+        texCoords = shiftFace(texCoords, -1, faces[1], temp, 'fromTemp')[0];
+    }
+
+    return texCoords;
+}
+
+function shiftFace(texCoords, fromDir, toDir, temp, tempDir) {
+    // default case
+    if(temp === undefined) {
+        // fromDir -> toDir
+        texCoords[toDir * 2] = texCoords[fromDir * 2];
+        texCoords[toDir * 2 + 1] = texCoords[fromDir * 2 + 1];
+    } else if(tempDir === 'fromTemp') {
+        // temp -> toDir
+        texCoords[toDir*2] = temp[0];
+        texCoords[toDir*2+1] = temp[1];
+    } else {
+        // fromDir -> temp
+        temp[0] = texCoords[fromDir*2];
+        temp[1] = texCoords[fromDir*2+1];
+    }
+
+    return [texCoords, temp];
+}
+
+function singleBlockVertices(ox, oy, oz, texCoords, facing = 'top') {
+    // for every not specified texture, take the previous one
+    texCoords.forEach((coord, index) => {
+        if(coord === -1) {
+            texCoords[index] = texCoords[index - 2]
+        }
+    })
+
+    /*
+    * Invert U (big to small and small to big) => mirror
+    * Invert V => mirror
+    * Go 1 down => rotate left in front / right in back
+    * */
+
+    let topVertices = [
+        //X, Y, Z,                         U, V
+        // Top
+        -1.0 + ox, 1.0 + oy, -1.0 + oz,    texCoords[1]/64,        texCoords[0]/32,
+        -1.0 + ox, 1.0 + oy, 1.0 + oz,     texCoords[1]/64,       (texCoords[0] + 1)/32,
+        1.0 + ox, 1.0 + oy, 1.0 + oz,      (texCoords[1] + 1)/64, (texCoords[0] + 1)/32,
+        1.0 + ox, 1.0 + oy, -1.0 + oz,     (texCoords[1] + 1)/64,  texCoords[0]/32
+    ]
+
+    let sidesVirtices = [];
+
+    // mirrored
+    sidesVirtices = sidesVirtices.concat([
+        //X, Y, Z,                         U, V
+        // Left
+        -1.0 + ox, 1.0 + oy, 1.0 + oz,     (texCoords[3] + 1)/64,  texCoords[2]/32,
+        -1.0 + ox, -1.0 + oy, 1.0 + oz,    (texCoords[3] + 1)/64, (texCoords[2] + 1)/32,
+        -1.0 + ox, -1.0 + oy, -1.0 + oz,   texCoords[3]/64,       (texCoords[2] + 1)/32,
+        -1.0 + ox, 1.0 + oy, -1.0 + oz,    texCoords[3]/64,        texCoords[2]/32
+    ])
+
+    sidesVirtices = sidesVirtices.concat([
+        //X, Y, Z,                         U, V
+        // Right
+        1.0 + ox, 1.0 + oy, 1.0 + oz,      texCoords[5]/64,        texCoords[4]/32,
+        1.0 + ox, -1.0 + oy, 1.0 + oz,     texCoords[5]/64,       (texCoords[4] + 1)/32,
+        1.0 + ox, -1.0 + oy, -1.0 + oz,    (texCoords[5] + 1)/64, (texCoords[4] + 1)/32,
+        1.0 + ox, 1.0 + oy, -1.0 + oz,     (texCoords[5] + 1)/64,  texCoords[4]/32
+    ])
+
+    sidesVirtices = sidesVirtices.concat([
+        //X, Y, Z,                         U, V
+        // Front
+        1.0 + ox, 1.0 + oy, 1.0 + oz,      (texCoords[7] + 1)/64,  texCoords[6]/32,
+        1.0 + ox, -1.0 + oy, 1.0 + oz,     (texCoords[7] + 1)/64, (texCoords[6] + 1)/32,
+        -1.0 + ox, -1.0 + oy, 1.0 + oz,    texCoords[7]/64,       (texCoords[6] + 1)/32,
+        -1.0 + ox, 1.0 + oy, 1.0 + oz,     texCoords[7]/64,        texCoords[6]/32
+    ])
+
+    sidesVirtices = sidesVirtices.concat([
+        //X, Y, Z,                         U, V
+        // Back
+        1.0 + ox, 1.0 + oy, -1.0 + oz,     texCoords[9]/64,        texCoords[8]/32,
+        1.0 + ox, -1.0 + oy, -1.0 + oz,    texCoords[9]/64,       (texCoords[8] + 1)/32,
+        -1.0 + ox, -1.0 + oy, -1.0 + oz,   (texCoords[9] + 1)/64, (texCoords[8] + 1)/32,
+        -1.0 + ox, 1.0 + oy, -1.0 + oz,    (texCoords[9] + 1)/64,  texCoords[8]/32
+    ]);
+
+    let bottomVertices = [
+        //X, Y, Z,                         U, V
+        // Bottom
+        -1.0 + ox, -1.0 + oy, -1.0 + oz,   (texCoords[11] + 1)/64,  texCoords[10]/32,
+        -1.0 + ox, -1.0 + oy, 1.0 + oz,    (texCoords[11] + 1)/64, (texCoords[10] + 1)/32,
+        1.0 + ox, -1.0 + oy, 1.0 + oz,     texCoords[11]/64,       (texCoords[10] + 1)/32,
+        1.0 + ox, -1.0 + oy, -1.0 + oz,    texCoords[11]/64,        texCoords[10]/32
+    ]
+
+    return topVertices.concat(sidesVirtices.concat(bottomVertices));
+}
