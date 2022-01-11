a54b6d6eed695a1fef4c76bef64fd7b503836bdf
ItamiKyojin <44494007+ItamiKyojin@users.noreply.github.com>
2022-01-11 17:55:03 +0100

    updated minecraft.js with better blocks array
    ... and other minor changes

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,6 +1,25 @@
 "use strict"
 
 
+/*
+* TODO
+*  -make Piston class and addPiston function
+*  .  (GitHub update)
+*  -Piston state-changes
+*  -animate Pistons and following Blocks
+*  -Piston logic
+*  .  (GitHub update)
+*  -textures for missing Redstone Components
+*  .  (GitHub update)
+*  -Redstone Component classes and functions
+*  .  (GitHub update)
+*  -Redstone logic
+*  .  (GitHub update)
+*  -AI Redstone construction
+*  .  (GitHub update)
+* */
+
+
 // ---------- define global constants ----------
 // enums
 const DIRECTION = {
@@ -11,8 +30,185 @@ const DIRECTION = {
     BACK : 4,
     BOTTOM : 5
 }
+const REDSTONE_DIRECTION = {
+    TOP : 0,
+    LEFT_TOP : 1,
+    LEFT : 2,
+    LEFT_BOTTOM : 3,
+    RIGHT_TOP : 4,
+    RIGHT : 5,
+    RIGHT_BOTTOM : 6,
+    FRONT_TOP : 7,
+    FRONT : 8,
+    FRONT_BOTTOM : 9,
+    BACK_TOP : 10,
+    BACK : 11,
+    BACK_BOTTOM : 12,
+    BOTTOM : 13
+}
 // math
 const DEG_TO_RAD = Math.PI/180;
+
+
+// ---------- define classes ----------
+class Array3d {
+    constructor(lengthX, lengthY, lengthZ) {
+        this.lengthX = lengthX;
+        this.lengthY = lengthY;
+        this.lengthZ = lengthZ;
+
+        this.array = new Array(lengthX * lengthY * lengthZ);
+    }
+
+    get(x, y, z) {
+        if(x >= this.lengthX || y >= this.lengthY || z >= this.lengthZ) {
+            return undefined;
+        }
+
+        return this.array[x  +  y * this.lengthX  +  z * this.lengthX * this.lengthY]
+    }
+
+    set(x, y, z, value) {
+        // maybe increase size
+        if(x >= this.lengthX || y >= this.lengthY || z >= this.lengthZ) {
+            let increaseBy = 5; // solid number to increase by
+            while(x >= this.lengthX+increaseBy) {
+                increaseBy += 5;
+            }
+            while(y >= this.lengthY+increaseBy) {
+                increaseBy += 5;
+            }
+            while(z >= this.lengthZ+increaseBy) {
+                increaseBy += 5;
+            }
+
+            let newArray3d = new Array3d(this.lengthX+increaseBy, this.lengthY+increaseBy, this.lengthZ+increaseBy);
+            let newX = -1;
+            let newY = -1;
+            let newZ = -1;
+            this.array.forEach((value, index) => {
+                newX = index % (this.lengthY * this.lengthX) % this.lengthX;
+                newY = ( (index - newX) % (this.lengthY * this.lengthX) ) / this.lengthX;
+                newZ = ( index - newX - newY*this.lengthX ) / this.lengthY / this.lengthX;
+
+                newArray3d.set(newX, newY, newZ, value);
+            })
+            this.lengthX = newArray3d.lengthX;
+            this.lengthY = newArray3d.lengthY;
+            this.lengthZ = newArray3d.lengthZ;
+            this.array = newArray3d.array;
+        }
+
+        // set value
+        this.array[x  +  y * this.lengthX  +  z * this.lengthX * this.lengthY] = value;
+    }
+}
+
+// indices can be negative
+class Array3dPlus {
+    constructor() {
+        this.array = new Array(8);
+        this.array[0] = new Array3d(6, 6, 6);
+        this.array[1] = new Array3d(5, 6, 6);
+        this.array[2] = new Array3d(6, 5, 6);
+        this.array[3] = new Array3d(5, 5, 6);
+        this.array[4] = new Array3d(6, 6, 5);
+        this.array[5] = new Array3d(5, 6, 5);
+        this.array[6] = new Array3d(6, 5, 5);
+        this.array[7] = new Array3d(5, 5, 5);
+    }
+
+    get(x, y, z) {
+        let arrayIndex;
+        [arrayIndex, x, y, z] = this.getPosNegArray(x, y, z);
+
+        return this.array[arrayIndex].get(x, y, z);
+    }
+
+    set(x, y, z, value) {
+        let arrayIndex;
+        [arrayIndex, x, y, z] = this.getPosNegArray(x, y, z);
+
+        this.array[arrayIndex].set(x, y, z, value);
+    }
+
+    getPosNegArray(x, y, z) {
+        let arrayIndex = 0;
+        if(x < 0) {
+            arrayIndex += 1;
+            x = -x - 1;
+        }
+        if(y < 0) {
+            arrayIndex += 2;
+            y = -y - 1;
+        }
+        if(z < 0) {
+            arrayIndex += 4;
+            z = -z - 1;
+        }
+
+        return [arrayIndex, x, y, z]
+    }
+}
+
+class Block {
+    constructor(name, meshes = [], transparent = false, powered = new Array(6), immovable = false) {
+        this.name = name;
+        this.meshes = meshes;
+        this.transparent = transparent;
+        this.powered = powered;
+        this.immovable = immovable;
+    }
+
+    power(direction) {
+        if(!this.transparent) {
+            this.powered[direction] = true;
+        }
+    }
+
+    depower(direction) {
+        if(!this.transparent) {
+            this.powered[direction] = false;
+        }
+    }
+}
+
+class RedstoneDust {
+    constructor(meshes = [], powered = new Array(14), maxPower = 0) {
+        this.meshes = meshes;
+        this.powered = powered;
+        this.maxPower = maxPower;
+    }
+
+    power(redstoneDirection, power) {
+        this.powered[redstoneDirection] = power;
+        this.updateMaxPower();
+    }
+
+    depower(redstoneDirection, power) {
+        this.powered[redstoneDirection] = power;
+        this.updateMaxPower();
+    }
+
+    updateMaxPower() {
+        let maxPower = 0;
+        this.powered.forEach(power => {
+            if(power > maxPower) {
+                maxPower = power;
+            }
+        })
+        this.maxPower = maxPower;
+    }
+}
+
+class Piston {
+    constructor(meshes = [], facing = DIRECTION.TOP, powered = false, extended = false) {
+        this.meshes = meshes;
+        this.facing = facing;
+        this.powered = powered;
+        this.extended = extended;
+    }
+}
 
 
 // do main
@@ -62,6 +258,8 @@ window.onload = (() => {
     const smoothStoneMaterial = new THREE.MeshStandardMaterial( {map : smoothStoneTexture} );
     // create multi faced materials
     const pistonMaterials = create6FacedMaterial(pistonTopTexture, pistonSideTexture, pistonSideTexture, pistonSideTexture, pistonSideTexture, pistonBottomTexture);
+    const pistonBaseMaterials = create6FacedMaterial(pistonInnerTexture, pistonSideTexture, pistonSideTexture, pistonSideTexture, pistonSideTexture, pistonBottomTexture);
+    const pistonHeadMaterials = create6FacedMaterial(pistonTopTexture, pistonSideTexture, pistonSideTexture, pistonSideTexture, pistonSideTexture, pistonTopTexture);
     // create multi colored material arrays
     const redstoneDustLine0MaterialArray = [];
     for(let i = 0; i < 16; i++) {
@@ -69,16 +267,66 @@ window.onload = (() => {
     }
 
 
-    // create geometry
+    // ---------- create geometry ----------
     const boxGeometry = new THREE.BoxGeometry();
-    const topPlaneGeometry = new THREE.PlaneGeometry();   // only visible from atop
+
+    const shaftGeometry = new THREE.BoxGeometry(0.25, 1, 0.25);
+    // change uv mapping of shaft, so that the sides use the top 25% of the given texture rotated counterclockwise
+    uvMap(shaftGeometry, 0.25, true, false, true);
+
+    // reminder to shift in one direction by 1/8, as it is not a full block
+    // generic name: box3_4BottomGeometry
+    const pistonBaseGeometry = new THREE.BoxGeometry(1, 0.75, 1);
+    // change uv mapping of pistonBase, so that the sides use the bottom 75% of the given texture
+    uvMap(pistonBaseGeometry, 0.75);
+
+    // reminder to shift in one direction by 3/8, as it is not a full block
+    // generic name: box1_4TopGeometry
+    const pistonHeadGeometry = new THREE.BoxGeometry(1, 0.25, 1);
+    // change uv mapping of pistonHead, so that the sides use the top 25% of the given texture
+    uvMap(pistonHeadGeometry, 0.25, true);
+
+    // only visible from atop
+    const topPlaneGeometry = new THREE.PlaneGeometry();
     topPlaneGeometry.rotateX(-90 * DEG_TO_RAD);
+
+
+    // initiate blocks array
+    const blocks = new Array3dPlus();
+
+
+    // ---------- define functions requiring specific textures, materials, geometry and/or blocks ----------
+    function addSmoothStoneBlock(x, y, z) {
+        blocks.set(
+            x, y, z,
+            new Block('smooth_stone', [addMeshToScene(scene, boxGeometry, smoothStoneMaterial, x, y, z)])
+        );
+    }
+
+    // should NOT be able to enter maxPower without powered[]
+    function addRedstoneDust(x, y, z, maxPower) {
+        blocks.set(
+            x, Math.round(y), z,
+            new RedstoneDust([addMeshToScene(scene, topPlaneGeometry, redstoneDustLine0MaterialArray[maxPower], x, y, z)])
+        );
+    }
+
+    function addPiston(x, y, z, facing = DIRECTION.TOP) {
+        blocks.set(
+            x, y, z,
+            new Piston([addMeshToScene(scene, boxGeometry, pistonMaterials, x, y, z, facing)], facing)
+        )
+    }
 
 
     // add 5x5 piston floor to scene
     for(let x = -2; x < 3; x++) {
         for(let z = -2; z < 3; z++) {
-            addMeshToScene(scene, boxGeometry, pistonMaterials, x, 0, z, DIRECTION.TOP);
+            //addSmoothStoneBlock(x, 0, z);
+            addPiston(x, 0, z);
+            /*addMeshToScene(scene, pistonHeadGeometry, pistonHeadMaterials, x, 1+3/8, z, DIRECTION.TOP);
+            addMeshToScene(scene, shaftGeometry, pistonMaterials, x, 3/4, z, DIRECTION.TOP);
+            addMeshToScene(scene, pistonBaseGeometry, pistonBaseMaterials, x, -1/8, z, DIRECTION.TOP);*/
         }
     }
 
@@ -110,7 +358,7 @@ window.onload = (() => {
         const x = document.querySelector('#x').value;
         const y = document.querySelector('#y').value;
         const z = document.querySelector('#z').value;
-        addMeshToScene(scene, boxGeometry, smoothStoneMaterial, x, y, z);
+        addSmoothStoneBlock(x, y, z);
     }
     addSmoothStoneBlockBtn.addEventListener('click', addSmoothStoneBlockEventListener)
 
@@ -119,19 +367,37 @@ window.onload = (() => {
     const addStuffBtn = document.querySelector('#add-stuff');
     const addStuffEventListener = () => {
         // add 5 smooth stone blocks before and after floor
+        let block = {'name' : 'smooth_stone', 'mesh' : undefined};
         for(let z = -7; z < -2; z++) {
-            addMeshToScene(scene, boxGeometry, smoothStoneMaterial, 0, 0, z);
+            addSmoothStoneBlock(0, 0, z);
         }
         for(let z = 3; z < 8; z++) {
-            addMeshToScene(scene, boxGeometry, smoothStoneMaterial, 0, 0, z);
+            addSmoothStoneBlock(0, 0, z);
         }
 
         // add redstone line powered 1 to 15
+        block.name = 'redstone_dust';
         for(let z = -7; z < 8; z++) {
-            addMeshToScene(scene, topPlaneGeometry, redstoneDustLine0MaterialArray[z+8], 0, 0.5 + 1/128, z);
+            addRedstoneDust(0, 0.5 + 1/64, z, z+8);
         }
     }
     addStuffBtn.addEventListener('click', addStuffEventListener);
+
+
+    // remove all meshes from scene on click
+    const removeMeshesBtn = document.querySelector('#remove-meshes');
+    const removeMeshesEventListener = () => {
+        removeAllMeshesFromScene(blocks, scene);
+    }
+    removeMeshesBtn.addEventListener('click', removeMeshesEventListener);
+
+
+    // add all meshes to scene on click
+    const addMeshesBtn = document.querySelector('#add-meshes');
+    const addMeshesEventListener = () => {
+        addAllMeshesToScene(blocks, scene);
+    }
+    addMeshesBtn.addEventListener('click', addMeshesEventListener);
 
 
     // animate
@@ -145,6 +411,46 @@ window.onload = (() => {
 
     animate();
 })
+
+// !WARNING! doRotate90DegreesCounterClockwise = true (currently) REQUIRES doCheckSidesOnly = false
+function uvMap(geometry, percentOfTexture, doTopPartOfTexture = false, doCheckSidesOnly = true, doRotate90DegreesCounterClockwise = false) {
+    let uv = geometry.getAttribute('uv');
+
+    // only take top 25% (4px) of piston_side.png
+    let checkSidesOnly = (index) => {
+        if(doCheckSidesOnly) return index < 16 || index >= 32;
+        return true;
+    }
+    let a = 1;
+    let b = 1;
+    if(doRotate90DegreesCounterClockwise) {
+        b = 0;
+    }
+    if(doTopPartOfTexture) {
+        a = 0;
+        percentOfTexture = 1 - percentOfTexture;
+    }
+    uv.array.forEach((value, index) => {
+        if(value === a && index%2 === b && checkSidesOnly(index)) {
+            uv.array[index] = percentOfTexture;
+        }
+    })
+
+    // rotate 90 degrees counterclockwise
+    if(doRotate90DegreesCounterClockwise) {
+        for (let i = 0; i < 1; i++) {
+            let temp = uv.array[0];
+            uv.array.forEach((value, index) => {
+                if (index !== 0) {
+                    uv.array[index - 1] = value;
+                }
+            })
+            uv.array[uv.array.length - 1] = temp;
+        }
+    }
+
+    geometry.setAttribute('uv', uv)
+}
 
 function loadTextureWithNearestFilter(loader, url) {
     const texture = loader.load(url);
@@ -170,60 +476,57 @@ function create6FacedMaterial(topTexture, leftTexture, rightTexture, frontTextur
 
 function getRedstoneColor(power) {
     // create redstone color constants
-    const BASE_REDSTONE_RED = 45;
-    const MULT_REDSTONE_RED = 14;
-    const MULT_REDSTONE_GREEN = 3;
-    const MULT_REDSTONE_BLUE = 3;
+    const BASE_REDSTONE_RED = 90;
+    const BASE_REDSTONE_GREEN = 15;
+    const BASE_REDSTONE_BLUE = 15;
+
+    const MULT_REDSTONE_RED = 11;
+    const MULT_REDSTONE_GREEN = 4;
+    const MULT_REDSTONE_BLUE = 4;
 
 
     // mix redstone color
     // rgb have to be multiplied by 256*256, 256 and 1 respectfully, so that they can be added together afterwards (rgb is hex and stuff, you know?)
-    return ((power*MULT_REDSTONE_RED+BASE_REDSTONE_RED)*256*256 + (power*MULT_REDSTONE_GREEN)*256 + (power*MULT_REDSTONE_BLUE));
-}
-
-// adds plane with redstone dust texture to scene
-// TODO proper, different textures
-function addRedstoneDustDotPlane(scene, power) {
-    // get texture and keep pixelated
-    const redstoneDustDotTexture = new THREE.TextureLoader().load('assets/minecraft/1.18.1/minecraft/textures/block/redstone_dust_line0.png');
-    redstoneDustDotTexture.magFilter = THREE.NearestFilter;
-    redstoneDustDotTexture.minFilter = THREE.NearestFilter;
-
-    // create plane geometry
-    const geometry = new THREE.PlaneGeometry();
-    geometry.rotateX(-90 * DEG_TO_RAD)
-
-    // set mesh parameters
-    const color = new THREE.Color((power*14+45)/255, (power*3)/255, (power*3)/255);
-    const material = new THREE.MeshStandardMaterial( {map : redstoneDustDotTexture, transparent : true, color : color.getHex()} )
-
-    // add mesh to scene
-    addMeshToScene(scene, geometry, material, 0, 0.5 + 1/128, 0);
+    return ((power*MULT_REDSTONE_RED+BASE_REDSTONE_RED)*256*256
+        + (power*MULT_REDSTONE_GREEN+BASE_REDSTONE_GREEN)*256
+        + (power*MULT_REDSTONE_BLUE+BASE_REDSTONE_BLUE));
 }
 
 function addMeshToScene(scene, geometry, material, x, y, z, facing = DIRECTION.TOP) {
-    const object = new THREE.Mesh(geometry, material);
+    const mesh = new THREE.Mesh(geometry, material);
 
     switch(facing) {
-        case DIRECTION.LEFT:
-            object.rotateZ(90 * DEG_TO_RAD)
-            break;
-        case DIRECTION.RIGHT:
-            object.rotateZ(-90 * DEG_TO_RAD)
-            break;
-        case DIRECTION.FRONT:
-            object.rotateX(90 * DEG_TO_RAD);
-            break;
-        case DIRECTION.BACK:
-            object.rotateX(-90 * DEG_TO_RAD);
-            break;
-        case DIRECTION.BOTTOM:
-            object.rotateX(180 * DEG_TO_RAD);
-            break;
+        case DIRECTION.LEFT: mesh.rotateZ(90 * DEG_TO_RAD); break;
+        case DIRECTION.RIGHT: mesh.rotateZ(-90 * DEG_TO_RAD); break;
+        case DIRECTION.FRONT: mesh.rotateX(90 * DEG_TO_RAD); break;
+        case DIRECTION.BACK: mesh.rotateX(-90 * DEG_TO_RAD); break;
+        case DIRECTION.BOTTOM: mesh.rotateX(180 * DEG_TO_RAD); break;
     }
 
-    object.position.set(x, y, z)
-    scene.add(object);
+    mesh.position.set(x, y, z)
+    scene.add(mesh);
+
+    return mesh;
+}
+
+function addAllMeshesToScene(blocks, scene) {
+    for(let i = 0; i < 8; i++) {
+        blocks.array[i].array.forEach(block => {
+            block.meshes.forEach(mesh => {
+                scene.add(mesh)
+            })
+        })
+    }
+}
+
+function removeAllMeshesFromScene(blocks, scene) {
+    for(let i = 0; i < 8; i++) {
+        blocks.array[i].array.forEach(block => {
+            block.meshes.forEach(mesh => {
+                scene.remove(mesh)
+            })
+        })
+    }
 }
 
 /*
